d51f7a348e2efc50ec43e59ba6bf21a2565f6643
Serhii Kalinichenko <kallinnas@gmail.com>
2021-04-27 19:01:45 +0300

    reduce anywhere cors

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://cors-anywhere.herokuapp.com/https://kupon-server.herokuapp.com/'
+  baseUrl: 'https://kupon-server.herokuapp.com/'
 };
